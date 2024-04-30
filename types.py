bce891a1de767bee7789b0511592ab33ca89e4ba
nomad <daffodil8900@gmail.com>
2024-04-30 12:33:23 -0400

    Added the class Person, function 'get_person_info', instance of Person 'person_1'

--- types.py
+++ types.py
@@ -19,3 +19,32 @@ var_UnionAlternative: bool | None | list
 
 # Literal type annotation is used when specific values for an argument(s) expected
 arg: dict[Literal["username"] | Literal["email"] | Literal["password"], str]
+
+""" Classes and dataclasses """
+
+class Person():
+    def __init__(self, person_id: int, age: int, name: str, email: str):
+        self.person_id = person_id
+        self.age = age
+        self.name = name
+        self.email = email
+
+def get_person_info(person: Person) -> str:
+    return f"{person.name} is {person.age} " \
+            f"years old. He/She has an email {person.email}"
+
+person_1: Person = Person(109387, 26, "Eugene", "eugene@gmail.com")
+print(get_person_info(person_1))
+
+
+
+
+
+
+
+
+
+
+
+
+
