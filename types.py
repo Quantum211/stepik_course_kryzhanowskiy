9c896a25edc5f75dbe7a3788975a34a712cd2d04
nomad <daffodil8900@gmail.com>
2024-04-30 14:26:10 -0400

    Added a new class 'EthernetProtocolStructure' with the implementation of the dataclasses library

--- types.py
+++ types.py
@@ -34,14 +34,30 @@ def get_person_info(person: Person) -> str:
             f"years old. He/She has an email {person.email}"
 
 person_1: Person = Person(109387, 26, "Eugene", "eugene@gmail.com")
-print(get_person_info(person_1))
+# print(get_person_info(person_1), end= "\n-------------------------------------------\n")
 
 
+from dataclasses import dataclass
 
+@dataclass(frozen= True)
+class EthernetProtocolStructure():
+    preamble: int
+    SFD: int
+    destination: int
+    source: int
+    type: int
+    data_and_padding: str
+    FCS: int
 
+    def __str__(self):
+        return f"Preamble: {self.preamble}\nSFD: {self.SFD}\nDestination: {self.destination}\nSource: {self.source}\n" \
+                f"Data and Padding: {self.data_and_padding}\nFCS: {self.FCS}"
 
-
-
+ether_1 = EthernetProtocolStructure(7, 1, 6, 6, 2, "46 - 1500", 4)
+# print(ether_1)
+""" Below assignment won't work because of the instance's frozen state."""
+# ether_1.data_and_padding = "1000"
+# print(ether_1)
 
 
 
